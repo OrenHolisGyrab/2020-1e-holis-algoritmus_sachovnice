cdb658dd9244cdae1520fb2bc886b185ee258da8
Oren Holiš <oren.holis@bronet.cz>
2021-05-01 19:50:43 +0200

    Adding more tests

--- Sachovnice/calculations/CalculationTests.java
+++ Sachovnice/calculations/CalculationTests.java
@@ -31,26 +31,247 @@ public class CalculationTests {
             createFigure(7, 2, 2, "pawn"),
             "Nikdy se nevyhodí"
         );
+
+        createTest(
+            createFigure(1, 1, 1, "pawn"),
+            createFigure(1, 2, 2, "pawn"),
+            "Nikdy se nevyhodí"
+        );
+
+        createTest(
+            createFigure(7, 0, 1, "pawn"),
+            createFigure(7, 7, 2, "king"),
+            "Nikdy se nevyhodí"
+        );
+
+        createTest(
+                createFigure(0, 0, 1, "pawn"),
+                createFigure(7, 7, 2, "king"),
+                "Nikdy se nevyhodí"
+        );
+
+        createTest(
+            createFigure(0, 0, 1, "pawn"),
+            createFigure(7, 6, 2, "king"),
+            "6"
+        );
+
+        createTest(
+            createFigure(0, 0, 1, "pawn"),
+            createFigure(2, 7, 2, "tower"),
+            "1"
+        );
+
+        createTest(
+            createFigure(0, 0, 1, "pawn"),
+            createFigure(3, 7, 2, "tower"),
+            "2"
+        );
+
+        createTest(
+            createFigure(0, 0, 1, "pawn"),
+            createFigure(4, 7, 2, "queen"),
+            "2"
+        );
+
+        createTest(
+            createFigure(0, 0, 1, "pawn"),
+            createFigure(2, 6, 2, "shooter"),
+            "3"
+        );
+
+        createTest(
+            createFigure(4, 6, 1, "pawn"),
+            createFigure(0, 0, 2, "horse"),
+            "Nikdy se nevyhodí"
+        );
+
+        createTest(
+            createFigure(4, 5, 1, "pawn"),
+            createFigure(0, 0, 2, "horse"),
+            "3"
+        );
     }
 
     public static void horse() {
         createTest(
+                createFigure(0, 0,1, "horse"),
+                createFigure(7, 7, 2, "horse"),
+                "3"
+        );
+
+        createTest(
+            createFigure(0, 1,1, "horse"),
+            createFigure(0, 3, 2, "horse"),
+            "1"
+        );
+
+        createTest(
             createFigure(0, 0,1, "horse"),
-            createFigure(7, 7, 2, "queen"),
+            createFigure(7, 0, 2, "king"),
+            "3"
+        );
+
+        createTest(
+            createFigure(0, 0,1, "horse"),
+            createFigure(4, 6, 2, "pawn"),
+            "3"
+        );
+
+        createTest(
+            createFigure(3, 2,1, "horse"),
+            createFigure(0, 0, 2, "pawn"),
+            "1"
+        );
+
+        createTest(
+            createFigure(0, 0,1, "horse"),
+            createFigure(4, 7, 2, "tower"),
+            "2"
+        );
+
+        createTest(
+            createFigure(0, 1,1, "horse"),
+            createFigure(4, 7, 2, "shooter"),
+            "2"
+        );
+
+        createTest(
+            createFigure(0, 0,1, "horse"),
+            createFigure(7, 6, 2, "shooter"),
             "1"
         );
     }
 
     public static void king() {
+        createTest(
+            createFigure(0, 0,1, "king"),
+            createFigure(7, 7, 2, "king"),
+            "4"
+        );
 
+        createTest(
+            createFigure(6, 0,1, "king"),
+            createFigure(7, 7, 2, "pawn"),
+            "6"
+        );
+
+        createTest(
+            createFigure(7, 0,1, "king"),
+            createFigure(7, 7, 2, "pawn"),
+            "Nikdy se nevyhodí"
+        );
+
+        createTest(
+                createFigure(0, 0,1, "king"),
+                createFigure(6, 6, 2, "tower"),
+                "2"
+        );
+
+        createTest(
+                createFigure(1, 6,1, "king"),
+                createFigure(4, 0, 2, "queen"),
+                "2"
+        );
+
+        createTest(
+                createFigure(0, 1,1, "king"),
+                createFigure(2, 6, 2, "shooter"),
+                "2"
+        );
+
+        createTest(
+            createFigure(0, 0,1, "king"),
+            createFigure(7, 7, 2, "horse"),
+            "3"
+        );
     }
 
     public static void shooter() {
+        createTest(
+            createFigure(0, 1,1, "shooter"),
+            createFigure(0, 0, 2, "shooter"),
+            "Nikdy se nevyhodí"
+        );
 
+        createTest(
+                createFigure(0, 0,1, "shooter"),
+                createFigure(5, 5, 2, "shooter"),
+                "1"
+        );
+
+        createTest(
+            createFigure(0, 0,1, "shooter"),
+            createFigure(7, 3, 2, "king"),
+            "2"
+        );
+
+        createTest(
+            createFigure(0, 0,1, "shooter"),
+            createFigure(1, 0, 2, "pawn"),
+            "2"
+        );
+
+        createTest(
+            createFigure(0, 0,1, "shooter"),
+            createFigure(6, 4, 2, "tower"),
+            "1"
+        );
+
+        createTest(
+            createFigure(0, 0,1, "shooter"),
+            createFigure(4, 4, 2, "queen"),
+            "1"
+        );
+
+        createTest(
+            createFigure(0, 1,1, "shooter"),
+            createFigure(4, 0, 2, "horse"),
+            "2"
+        );
     }
 
     public static void queen() {
+        createTest(
+                createFigure(1, 6,1, "queen"),
+                createFigure(1, 4, 2, "queen"),
+                "1"
+        );
 
+        createTest(
+            createFigure(0, 0,1, "queen"),
+            createFigure(7, 2, 2, "pawn"),
+            "2"
+        );
+
+        createTest(
+                createFigure(0, 0,1, "queen"),
+                createFigure(7, 6, 2, "pawn"),
+                "1"
+        );
+
+        createTest(
+                createFigure(1, 6,1, "queen"),
+                createFigure(1, 4, 2, "tower"),
+                "1"
+        );
+
+        createTest(
+                createFigure(0, 3,1, "queen"),
+                createFigure(7, 0, 2, "horse"),
+                "2"
+        );
+        createTest(
+                createFigure(0, 3,1, "queen"),
+                createFigure(7, 3, 2, "shooter"),
+                "1"
+        );
+
+        createTest(
+                createFigure(0, 1,1, "queen"),
+                createFigure(7, 3, 2, "king"),
+                "2"
+        );
     }
 
     public static void tower() {
@@ -89,6 +310,18 @@ public class CalculationTests {
             createFigure(7, 7, 2, "king"),
             "2"
         );
+
+        createTest(
+                createFigure(1, 6,1, "tower"),
+                createFigure(6, 4, 2, "pawn"),
+                "2"
+        );
+
+        createTest(
+                createFigure(0, 1,1, "tower"),
+                createFigure(7, 4, 2, "pawn"),
+                "2"
+        );
     }
 
     public static void main(String[] args) {
@@ -104,31 +337,6 @@ public class CalculationTests {
         horse();
         System.out.println("-----------------------Shooter-----------------------------");
         shooter();
-
-        createTest(
-            createFigure(0, 0, 1, "horse"),
-            createFigure(6, 3, 2, "horse"),
-            "2"
-        );
-
-        createTest(
-            createFigure(1, 1, 1, "shooter"),
-            createFigure(7, 7, 2, "pawn"),
-            "1"
-        );
-
-        createTest(
-            createFigure(1, 0, 1, "tower"),
-            createFigure(4, 4, 2, "tower"),
-            "1"
-        );
-
-
-        createTest(
-            createFigure(1, 0, 1, "shooter"),
-            createFigure(1,1, 2, "shooter"),
-            "Nikdy se nevyhodí"
-        );
     }
 
     private static InsertedFigure createFigure(int x, int y, int figureNumber, String typ) {
@@ -147,8 +355,7 @@ public class CalculationTests {
             }
             System.out.println("test " + testNumber  + " passed");
         } catch (Exception ex) {
-            System.out.println("fail");
-            System.out.println("test " + testNumber);
+            System.out.println(ex);
         }
     }
 }
